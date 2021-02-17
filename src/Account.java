a3eff25b33bbdfd37d6cc11a1cc42b21f4331397
kcheng902 <47684688+kcheng902@users.noreply.github.com>
2021-02-17 13:01:36 -0800

    account class

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,37 @@
+import java.util.*;
 
 public class Account {
+	private int id;
+	private int bal;
+	private int trans;
 
+	public Account(int id, int bal, int trans) {
+		this.id = id;
+		this.bal = bal;
+		this.trans = trans;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public int getBal() {
+		return this.bal;
+	}
+
+	public int getTrans() {
+		return this.trans;
+	}
+
+	public void setBal(int b) {
+		this.bal = b;
+	}
+
+	public void setTrans(int t) {
+		this.trans = t;
+	}
+
+	public String toString() {
+		return ("acct:" + id + " bal:" + bal + " trans:" + trans);
+	}
 }
