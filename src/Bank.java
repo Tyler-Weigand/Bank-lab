308b8fc36de7235f751a849f8d3f87dc774dc1a1
Tyler-Weigand <tweigand1@hwemail.com>
2021-02-17 13:22:45 -0800

    inner classes

--- src/Bank.java
+++ src/Bank.java
@@ -1,8 +1,15 @@
 import java.util.*;
 
+
 public class Bank {
-	private static ArrayDeque transactions = new ArrayDeque();
-	private static int numWorkers = 20;
+	private ArrayDeque transactions = new ArrayDeque();
+	private int numWorkers = 20;
+	class Worker implements Runnable{
+		public void run() {
+			numWorkers++;
+		}
+	}
+	
 	public static void main (String [] args) {
 		System.out.println("lol");
 	}
