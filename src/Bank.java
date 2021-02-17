06ec22f0e6937e9489e007e0b0755f322fd5df14
Tyler-Weigand <tweigand1@hwemail.com>
2021-02-17 13:14:17 -0800

    made static

--- src/Bank.java
+++ src/Bank.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
 public class Bank {
-	private ArrayDeque transactions = new ArrayDeque();
-	private int numWorkers = 20;
+	private static ArrayDeque transactions = new ArrayDeque();
+	private static int numWorkers = 20;
 	public static void main (String [] args) {
-		
+		System.out.println("lol");
 	}
 }
