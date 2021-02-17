08eaeaa5a29697614540d2531a55e7a47bdf5201
Tyler-Weigand <tweigand1@hwemail.com>
2021-02-17 13:12:26 -0800

    variables for bank

--- src/Bank.java
+++ src/Bank.java
@@ -1,4 +1,9 @@
+import java.util.*;
 
 public class Bank {
-
+	private ArrayDeque transactions = new ArrayDeque();
+	private int numWorkers = 20;
+	public static void main (String [] args) {
+		
+	}
 }
