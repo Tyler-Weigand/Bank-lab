0f03350464c9e3ae5227c139d39599fd850fa113
kcheng902 <47684688+kcheng902@users.noreply.github.com>
2021-02-17 13:30:53 -0800

    bank: open worker threads

--- src/Bank.java
+++ src/Bank.java
@@ -4,13 +4,18 @@ import java.util.*;
 public class Bank {
 	private ArrayDeque transactions = new ArrayDeque();
 	private int numWorkers = 20;
-	class Worker implements Runnable{
+
+	class Worker implements Runnable {
 		public void run() {
 			numWorkers++;
 		}
 	}
 	
 	public static void main (String [] args) {
-		System.out.println("lol");
+		String filename = args[0];
+		int threads = Integer.parseInt(args[1]);
+		for (int i = 0; i<threads; i++) {
+			(new Thread(new Worker())).start();
+		}
 	}
 }
