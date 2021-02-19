c353c819ac60de4fe992e596f8a0e1229aa388bc
kcheng902 <47684688+kcheng902@users.noreply.github.com>
2021-02-19 10:37:36 -0800

    initialize accounts sort of

--- src/Bank.java
+++ src/Bank.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Bank {
 	private ArrayDeque transactions = new ArrayDeque();
-	private int numWorkers = 20;
+	private int numAccounts = 20;
 
 	class Worker implements Runnable {
 		public void run() {
@@ -12,10 +12,21 @@ public class Bank {
 	}
 	
 	public static void main (String [] args) {
+		//initialize acccounts
+		Account[] accounts = new Account[20];
+		for (int i=0; i<20; i++) {
+			accounts[i] = new Account();
+		}
+
+		//read args
 		String filename = args[0];
 		int threads = Integer.parseInt(args[1]);
+
+		//initialize worker threads
 		for (int i = 0; i<threads; i++) {
 			(new Thread(new Worker())).start();
 		}
+
+
 	}
 }
