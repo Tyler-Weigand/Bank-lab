d7ab3426e43efd6ee8e53deff62bf5b8b98d8e3b
kcheng902 <47684688+kcheng902@users.noreply.github.com>
2021-02-19 10:38:37 -0800

    account private instance var

--- src/Bank.java
+++ src/Bank.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class Bank {
 	private ArrayDeque transactions = new ArrayDeque();
 	private int numAccounts = 20;
+	private Account[] accounts = new Account[20];
+
 
 	class Worker implements Runnable {
 		public void run() {
@@ -13,7 +15,6 @@ public class Bank {
 	
 	public static void main (String [] args) {
 		//initialize acccounts
-		Account[] accounts = new Account[20];
 		for (int i=0; i<20; i++) {
 			accounts[i] = new Account();
 		}
