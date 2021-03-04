b3d3b75cb72e66223c95bab56bc4b3e8090de899
Tyler-Weigand <tweigand1@hwemail.com>
2021-03-04 09:25:56 -0800

    got rid of weird comments

--- src/Bank.java
+++ src/Bank.java
@@ -6,8 +6,7 @@ import java.io.*;
 public class Bank {
 	private static int numAccounts = 20;
 	private static ArrayBlockingQueue<Transaction> transactions = new ArrayBlockingQueue<Transaction>(1000);
-	private static Account[] accounts = new Account[20];
-	//private static int numWorkers = 0;
+	private static Account[] accounts = new Account[numAccounts];
 
 
 	class Worker implements Runnable{
@@ -15,31 +14,21 @@ public class Bank {
 			while(true) {
 				try {
 					Transaction trans = transactions.take();
-					//System.out.println("heyol");
 					if(trans.getSender() == -1) { //if we got an exit sequence
-						//System.out.println("hey");
 						Num.change();
-						//System.out.println(numWorkers);
 						return;
 					}
 					else { //process transaction
-						//System.out.println("heyo");
 						Account sender = accounts[trans.getSender()];
 						Account receiver = accounts[trans.getReceiver()];
 						sender.withdraw(trans.getAmount());
 						receiver.deposit(trans.getAmount());
-						//System.out.println(sender.getBal());
 					}
-					//System.out.println("test");
 				} catch(Exception E) {
 					
 				}
 			}
 		}
-
-		/*public synchronized void changeWorkers() {
-			numWorkers--;
-		}*/
 	}
 	
 	public static void main (String [] args) throws IOException, InterruptedException{
@@ -76,7 +65,6 @@ public class Bank {
 		for (int i=0; i<temp; i++) {
 			transactions.put(new Transaction(-1,-1,-1));
 		}
-		//System.out.println("lol");
 		while (Num.numWorkers > 0) {
 			try {
 				Thread.sleep(100);
@@ -85,33 +73,9 @@ public class Bank {
 				throw new RuntimeException(e);
 			}
 		}
-		//System.out.println("lolo");
 		for (Account acc : accounts) {
 			System.out.println(acc);
 		}
+		s.close();
 	}
 }
-/*
- * acct:0 bal:1000 trans:10360
-acct:1 bal:1000 trans:8880
-acct:2 bal:1000 trans:10440
-acct:3 bal:1000 trans:9840
-acct:4 bal:1000 trans:10520
-acct:5 bal:1000 trans:10520
-acct:6 bal:1000 trans:9480
-acct:7 bal:1000 trans:9440
-acct:8 bal:1000 trans:8720
-acct:9 bal:1000 trans:9000
-acct:10 bal:1000 trans:9960
-acct:11 bal:1000 trans:10520
-acct:12 bal:1000 trans:9760
-acct:13 bal:1000 trans:9640
-acct:14 bal:1000 trans:10320
-acct:15 bal:1000 trans:9840
-acct:16 bal:1000 trans:10400
-acct:17 bal:1000 trans:10560
-acct:18 bal:1000 trans:11720
-acct:19 bal:1000 trans:10080
- */
-
-
